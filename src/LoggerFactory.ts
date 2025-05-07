65cff73521b1fa74d2020de71441678d6b439b67
sunilmurugesan <smurugesan@companieshouse.gov.uk>
2025-05-07 07:48:35 +0100

    CC-2096 - Instrumentation changes to report logs to open collector

--- src/LoggerFactory.ts
+++ src/LoggerFactory.ts
@@ -5,6 +5,15 @@ import StructuredLogger from "./StructuredLogger";
 import config from "./config";
 import logLevels from "./levelConfig";
 import winston from "winston";
+import * as api from "@opentelemetry/api-logs";
+// It might be one of http, proto, grpc depending on preferred transport
+import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
+
+import {
+  LoggerProvider,
+  SimpleLogRecordProcessor
+} from "@opentelemetry/sdk-logs";
+import { OpenTelemetryTransportV3 } from "@opentelemetry/winston-transport";
 
 class LoggerFactory {
 
@@ -21,10 +30,30 @@ class LoggerFactory {
 
         winston.addColors(logLevels.colours);
 
+
+        // const logExporter = new OTLPLogExporter();
+        // const resource: IResource = detectResourcesSync({
+        //   // this have to be manually adjusted to match SDK OTEL_NODE_RESOURCE_DETECTORS
+        //       detectors: [envDetectorSync, processDetectorSync, hostDetectorSync],
+        //     });
+
+        
+        const loggerProvider = new LoggerProvider();
+
+        loggerProvider.addLogRecordProcessor(
+          // new BatchLogRecordProcessor(logExporter)
+          new SimpleLogRecordProcessor(new OTLPLogExporter())
+          // new SimpleLogRecordProcessor(new ConsoleLogRecordExporter())
+        );
+        api.logs.setGlobalLoggerProvider(loggerProvider);
+
         return winston.createLogger({
             level: config.level,
             levels: logLevels.levels,
-            transports: [new winston.transports.Console(this.createTransportOptions(options.namespace))],
+            transports: [
+              new winston.transports.Console(this.createTransportOptions(options.namespace)),
+              new OpenTelemetryTransportV3()
+            ],
             exitOnError: false
         }) as StructuredLogger;
     }
