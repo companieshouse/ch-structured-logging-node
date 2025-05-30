b951633b18f5660e20b7115253859503ba557a34
sunilmurugesan <smurugesan@companieshouse.gov.uk>
2025-05-30 08:20:12 +0100

    CC-2133 - Node open telemetry spike

--- src/LoggerFactory.ts
+++ src/LoggerFactory.ts
@@ -5,11 +5,11 @@ import StructuredLogger from "./StructuredLogger";
 import config from "./config";
 import logLevels from "./levelConfig";
 import winston from "winston";
-import api from "@opentelemetry/api-logs";
+import * as api from "@opentelemetry/api-logs";
 import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-http";
-const { envDetector, processDetector, hostDetector, detectResourcesSync } = require('@opentelemetry/resources');
 import { LoggerProvider, BatchLogRecordProcessor } from "@opentelemetry/sdk-logs";
 import { OpenTelemetryTransportV3 } from "@opentelemetry/winston-transport";
+const { envDetector, processDetector, hostDetector, detectResourcesSync } = require('@opentelemetry/resources');
 
 class LoggerFactory {
 
@@ -25,6 +25,9 @@ class LoggerFactory {
   public static create(options: LoggerOptions) {
 
     winston.addColors(logLevels.colours);
+
+    console.log('config.otelLogEnabled', config.otelLogEnabled);
+
 
     if (config.otelLogEnabled) {
       const loggerProvider = new LoggerProvider({
@@ -44,6 +47,9 @@ class LoggerFactory {
       new winston.transports.Console(this.createTransportOptions(options.namespace)),
       ...(config.otelLogEnabled ? [new OpenTelemetryTransportV3()] : [])
     ];
+
+    console.log('transports', transports);
+    
 
     return winston.createLogger({
       level: config.level,
