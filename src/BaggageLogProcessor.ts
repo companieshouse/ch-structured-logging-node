9ed4ceb800052f61bffd439bbadb28e81766e86d
sunilmurugesan <smurugesan@companieshouse.gov.uk>
2025-07-29 14:32:59 +0100

    CC-2579 - Baggage PoC.

--- src/BaggageLogProcessor.ts
+++ src/BaggageLogProcessor.ts
@@ -3,9 +3,13 @@ import { context } from '@opentelemetry/api';
 
 const { LogRecordProcessor } = require('@opentelemetry/sdk-logs');
 
-class BaggageLogRecordProcessor extends LogRecordProcessor {
+class BaggageLogRecordProcessor extends LogRecordProcessor {  
   onEmit(logRecord: any) {
+    console.log("WOOOT !!!! Inside Log record processor");
+    
     const baggage = propagation.getBaggage(context.active());
+    console.log('Baggage received is ', baggage);
+    
     if (baggage) {
       for (const [key, entry] of baggage.getAllEntries()) {
         logRecord.attributes[`baggage.${key}`] = entry.value;
