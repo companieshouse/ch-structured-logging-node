59981b41dc98acb8e49d9dd174c2dce090ad7d38
sunilmurugesan <smurugesan@companieshouse.gov.uk>
2025-07-30 16:58:50 +0100

    CC-2717 - Log trace id and span id.

--- src/formatting/JsonFormatFactory.ts
+++ src/formatting/JsonFormatFactory.ts
@@ -1,11 +1,26 @@
 import moment from "moment";
 import winston from "winston";
+const { trace } = require('@opentelemetry/api');
 
 class JsonFormatFactory {
 
     public static create(namespace: string) {
 
         return winston.format.printf(function (info) {
+
+            const span = trace.getSpan(trace.context.active());
+
+            console.log("Span is : ", span);
+
+            const traceContext = span ? span.spanContext() : {};
+
+            console.log("Trace context is : ", traceContext);
+
+            const traceId = traceContext?.traceId || 'N/A';
+            const spanId = traceContext?.spanId || 'N/A';
+
+            console.log("Trace id : ", traceId);
+            console.log('Span id: ', spanId);
 
             const message = {
                 created: moment().format("YYYY-MM-DDTHH:mm:ss.SSSZ"),
