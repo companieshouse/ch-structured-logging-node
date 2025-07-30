0304238d0a0a4752755ed6dbf4fff09b6966f2c2
sunilmurugesan <smurugesan@companieshouse.gov.uk>
2025-07-30 17:58:23 +0100

    CC-2717 - Log trace id and span id.

--- src/formatting/JsonFormatFactory.ts
+++ src/formatting/JsonFormatFactory.ts
@@ -6,13 +6,15 @@ class JsonFormatFactory {
     public static create(namespace: string) {
 
         return winston.format.printf(function (info) {
-            console.log("Info", info);
+            console.log("Info: ", info);
             
             const message = {
                 created: moment().format("YYYY-MM-DDTHH:mm:ss.SSSZ"),
                 event: info.level,
                 namespace: namespace,
                 context: info.context,
+                trace_id: info.trace_id,
+                span_id: info.span_id,
                 data: {
                     message: info.message,
                     path: info.path,
