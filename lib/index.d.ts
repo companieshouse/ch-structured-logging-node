8c301fcaa7495374e692261ec7ac4263e654b6f2
sunilmurugesan <smurugesan@companieshouse.gov.uk>
2026-01-23 16:08:42 +0000

    ASM‑831: Revert versions for Testing.

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,5 @@
+/// <reference types="qs" />
+/// <reference types="express" />
 import ApplicationLogger from "./ApplicationLogger";
 declare const createLoggerMiddleware: (namespace: string) => import("express").RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("qs").ParsedQs, Record<string, any>>;
 declare const createLogger: (namespace: string) => ApplicationLogger;
